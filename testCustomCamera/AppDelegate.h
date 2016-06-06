016ae9cd0e1edbbfafba045e0f062644197df071
wangyingbo <wangyingbo528@126.com>
2016-06-06 12:43:40 +0800

    提交

---
+++ testCustomCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCustomCamera
+//
+//  Created by 王迎博 on 16/6/6.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testCustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCustomCamera
+//
+//  Created by 王迎博 on 16/6/6.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
